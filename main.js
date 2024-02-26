cd236032f33faecf2888d009392817913529613b
Daman Ahuja <142963733+DamanAhuja@users.noreply.github.com>
2024-02-26 14:59:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+import * as THREE from '/three.module.js';
+import {OrbitControls} from '/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls(camera,renderer.domElement);
+const geometry = new THREE.TorusGeometry(2 , 0.2, 10, 100 );
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const torus = new THREE.Mesh( geometry, material );
+scene.add( torus );
+
+const planegeometry = new THREE.PlaneGeometry(5,5);
+const planematerial = new THREE.MeshBasicMaterial( {color: 0x00ff00});
+const plane = new THREE.Mesh(planegeometry,planematerial);
+scene.add(plane);
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+    plane.rotation.x += 0.01;
+	plane.rotation.y += 0.01;
+	torus.rotation.x += 0.01;
+	torus.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate();
